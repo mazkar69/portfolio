bd28aa4e3736040b3d4603d452eb2ad4651851fb
AzKaR <mohdazkar@outlook.com>
2022-06-28 09:45:48 +0530

    shift to node with new features

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+const ejs = require("ejs")
+const port = process.env.PORT || 5000;
+
+// Seting the static file
+app.use("/static",express.static("static"))
+
+// Seting the view engine
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req,res)=>{
+    res.render("index")
+    
+})
+
+app.listen(port,()=>{
+    console.log("Running at port ",port);
+})
+
